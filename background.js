448718aa43afd769ee7d00ac37756d8c5f178399
2276225819 <2276225819@qq.com>
2016-09-02 04:07:28 +0800

    init

---
+++ background.js
@@ -0,0 +1,117 @@
+chrome.extension.getBackgroundPage().oauth = new OAuth({
+    'auth_url':'https://login.live.com/oauth20_authorize.srf',
+    'token_url':'https://login.live.com/oauth20_token.srf',
+    //'redirect_url':'https://www.getpostman.com/oauth2/callback',//使用服务器预设
+    'appid':'5488e2a9-2c68-4185-9b04-b5218dcad5c1',
+    'secret':'B6vPLrYY2xZ6qhzqpCdhnH2',
+    'scope':'wl.skydrive wl.skydrive_update ', 
+});   
+
+
+function GET(name,path){ 
+     var reg = new RegExp("(^|\\\?|&)"+ name +"=([^&]*)(&|$)");
+     var r = (path||window.location.search).substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
+function OAuth(opt){     
+    var $ = null;
+    var _this=this;
+    _this.getOAuthURL=function(){  
+        return opt.auth_url
+                +'?client_id='+opt.appid
+                +'&scope='+opt.scope
+                +'&response_type='+'code' //*/
+    } ;  　
+    _this.getToken=function(success,jquery){ //function success(auth,user):null  
+        $ = jquery;
+        var data = JSON.parse(localStorage['data']||'{}'); 
+        if(data && data.access_token){
+            testToken(data ,success);
+        }else{   
+            getCode('new login',success); 
+        } 
+    }   
+    function createToken(code, success  ){  
+        $.ajax({
+            url:opt.token_url,  
+            type:"POST",
+            data:{
+                client_id:opt.appid,   client_secret:opt.secret,
+                code:code,  grant_type:'authorization_code'
+            },
+            contentType:"application/x-www-form-urlencoded",
+            dataType:"json",
+            success: function(data){
+                testToken(data ,success);  
+                localStorage['data'] = JSON.stringify(data);
+                _this.data = data; 
+            },
+            error:function(e){  
+                if( e.status == 0 ) return; //非服务器返回错误 
+                setTimeout(function(){
+                    getCode('error login '+ e.statusText||e.responseText,success);  
+                },1000);
+            }
+        });  
+    }
+    function getCode(name,success){ 
+        var bgp = chrome.extension.getBackgroundPage();
+        var url =  _this.getOAuthURL()
+        bgp.openDialog( url ,name, function T(code){  
+            createToken(code,success);
+        }); 
+    }
+    function testToken(data,success){
+        var token = data.access_token;
+        var user = JSON.parse(localStorage['user']||'{}'); 
+        if(data && user) 
+            success(data,user);  
+        $.ajax({
+            url:'https://apis.live.net/v5.0/me?access_token='+token,
+            success:function(user){
+                _this.info = user;
+                if(!user) success(data,user); 
+                localStorage['user'] = JSON.stringify(user); 
+                
+                _exlog("...success...") ; 
+            },
+            error:function(e){  
+                if( e.status == 0 ) return; //非服务器返回错误
+                setTimeout(function(){ 
+                    delete localStorage['data'] ;
+                    getCode( 'expire login '+  e.statusText||e.responseText, success );  
+                },1000);
+            }
+        });  
+    }
+}
+ 
+
+
+ 
+function _exlog(str){ 
+    console.log(str);
+}
+chrome.extension.getBackgroundPage().log=function(str){ 
+    console.log(str);
+} 
+
+//背景线程执行
+chrome.extension.getBackgroundPage().openDialog = function(url,title,success){
+    _exlog("open1:"+title);
+    chrome.windows.create({  url:url, type:'popup',width:400,height:600,top:0 },function(win){   
+        chrome.windows.update(win.id,{focused:true});  
+        var tabid = win.tabs[0].id; 
+        setTimeout(function T(){ 
+            _exlog("check:"+title);
+            chrome.tabs.get(tabid,function(d){
+                if(!d) return;
+                var code = GET('code',d.url);
+                if(!code)return setTimeout(T,1000); 
+                _exlog("stop:"+title);
+                chrome.tabs.remove(tabid);
+                success(code);  
+            }) 
+        },1000); 
+    }); 
+}
---
+++ script.js
@@ -0,0 +1,235 @@
+//_exlog("pageload");   
+$(function(){  
+    var $ul = $('#list').html('正在登陆,请不要关闭窗口...'); 
+    _exLoginLive(function(data,user){  
+        $('#tool>span').html(user.name); 
+        success($ul,data);  
+    });    
+});  
+window.onerror=function(e){ 
+    chrome.extension.getBackgroundPage().log(str);
+};
+
+function _exlog(str){ 
+    chrome.extension.getBackgroundPage().log(str);
+}
+function _exLoginLive(cb){ 
+    chrome.extension.getBackgroundPage().oauth.getToken(cb,$);
+} 
+
+function success($ul,data){
+    $ul.html('正在加载目录..');
+    var d = new OneDriver(data.access_token);    
+    d.favorites(function(list,src){ 
+        $('#tool>button').attr('data-add',src); 
+        render($ul,list,d);  
+    });  
+
+    
+    $(document).unbind()
+    .on('click','#tool>a',function(){
+        chrome.tabs.create({url:'https://onedrive.live.com/',selected:true}); 
+    })
+    .on('click','#tool>i', function(){ 
+        delete localStorage['data'] ;
+        delete localStorage['code'] ;
+    })
+    .on('click','[data-file]',function(){
+        var src = $(this).data('file');
+        chrome.tabs.create({url:src,selected:true});
+        return false;
+    })
+    .on('click','[data-folder]',function(){ 
+        var src = $(this).data('folder'); 
+        var $u = $(this).find('+ul');
+        if($u.html()) return $u.html("");
+        $u.html('加载中...'); 
+        d.goto(src,function(list){  
+            render($u,list,d);  
+        });
+        return false; 
+    }) 
+    .on('click','[data-add]',function(){ 
+        var src = $(this).data('add');
+        var $u = $(this).parent().find('>ul');
+        chrome.tabs.getSelected(function(tabs){
+            var url = tabs.url; 
+            var title = window.prompt("添加新书签："+url,tabs.title);
+            if(title) {
+                $('<li>加载中...</li>').attr('style','opacity:0.3').appendTo($u);; 
+                d.add(src,title,url,function(){   
+                    d.goto(src,function(list){  
+                        render($u,list,d);  
+                    });
+                });   
+            }
+        });  
+        return false;
+    })
+    .on('click','[data-del]',function(){
+        var $this = $(this).parent().attr('style','opacity:0.3');
+        var src = $(this).data('del'); 
+        d.del(src,function(data,dir){  
+            $this.remove();
+        });   
+    })
+}
+function render($ul,data,d){  
+    $ul.empty();
+    for(var k in data) with({ item:data[k], $li:$('<li></li>').appendTo($ul)   }) {   
+        if(item.type=='folder'){ 
+            $li.attr('class',item.type).append('<ul></ul>'); 
+            $('<div>'+item.name+'</div>').prependTo($li)
+                .attr('data-folder',item.upload_location);
+            $('<button class="add" title="添加新书签"></button>').prependTo($li)
+                .attr('data-add',item.upload_location); 
+        }      
+        if(item.type=='file'){ 
+            var name = item.name.replace(/\.url$/,'');
+            var $del = $('<button class="del" title="删除书签"></button>').prependTo($li)
+            $li.append('<div>'+ name +'</div>');  
+            d.load(item.upload_location,function(html){
+                var arr = html.match(/URL=([^\n]*)/);
+                $del.attr('data-del',item.upload_location); 
+                $li.attr('class',item.type)
+                    .find('>div') .attr('data-file', arr[1] );  
+            });   
+        } 
+    } 
+} 
+
+function OneDriver(token){
+    var _base='';
+    var _this = this;
+    _this.token=token;
+    _this.favorites=function T(cb){     
+        if(localStorage['favorites']) 
+            success( JSON.parse(localStorage['favorites']) );  
+        if(!T['favorites']){
+            T['favorites']=true;
+            $.get("https://apis.live.net/v5.0/me/skydrive?access_token="+token,function(json){
+                var base_url = json.upload_location;
+                $.get(base_url+"?access_token="+token,function(json){  
+                    localStorage['favorites_url'] = base_url;
+                    if(!localStorage['favorites']) success(json);
+                    localStorage['favorites'] = JSON.stringify(json); 
+
+                    setTimeout(function(){
+                        T['favorites']=false;
+                    },60*1000); 
+
+                });
+            });
+        }
+        function success(json){ 
+            console.log(json);
+            for(var k in json.data){
+                var item = json.data[k];
+                if(item.name=='favorites')
+                    return _this.goto(item.upload_location,cb);
+            } 
+            //alert('error');  return; 
+            $.ajax({
+                url:localStorage['favorites_url'].toString().replace(/\/files\/$/,'') , 
+                type:'POST', 
+                headers:{ "Authorization":"Bearer "+token,'Content-Type': 'application/json' },
+                data: '{"name":"favorites"}',
+                success:function(json){
+                    return _this.goto(json.upload_location ,cb);
+                }
+            })  
+        };
+    } 
+    _this.goto=function T(upload_location,cb){  
+        _base = upload_location;  
+        if(localStorage[upload_location]) 
+            cb(JSON.parse(localStorage[upload_location]),upload_location); 
+ 
+        if(!T[upload_location]){
+            T[upload_location]=true;
+            $.ajax({ 
+                url: upload_location+"?access_token="+token,
+                success:function(json){
+                    if(!localStorage[upload_location]) cb(json.data,upload_location);
+                    localStorage[upload_location] = JSON.stringify(json.data);
+                    setTimeout(function(){
+                        T[upload_location]=false;
+                    },60*1000); 
+                },
+                error:function(e){  
+                    _exlog(e);
+                    //_this._clear();  
+                    //cb({},upload_location);
+                }
+            });  
+        }
+    }
+    _this.load=function T(upload_location,cb){
+        if(localStorage[upload_location]) 
+            cb(JSON.parse(localStorage[upload_location]));
+ 
+        if(!T[upload_location]){
+            T[upload_location]=true;
+            $.get(upload_location+'?access_token='+token,function(data){
+                if(!localStorage[upload_location])  cb( data );
+                localStorage[upload_location] = JSON.stringify(data);
+                setTimeout(function(){
+                    T[upload_location]=false;
+                },60*1000); 
+            });
+        }
+    }
+    _this.add=function(upload_location, name, url,success ){
+        var file = name+'.url'; 
+        [/\\\\/g, /\//g, /:/g, /;/g, /\*/g, /</g, />/g, /\|/g, /\?/g].forEach(function(v){
+            file = file.replace(v,'');
+        }); 
+        var text = 'URL='+url; 
+        $.ajax({
+            url: upload_location +'?access_token='+ token,
+            type:'POST',
+            contentType : 'multipart/form-data; boundary=EEEEEEEEEEEEEEEEEEEE',
+            processData : false,  
+            data:'--EEEEEEEEEEEEEEEEEEEE\r\nContent-Disposition: form-data; name="file"; filename="'+file+'"\r\nContent-Type: application/octet-stream\r\n\r\n[InternetShortcut]\r\n'+text+'\r\nRoamed=-1\r\n--EEEEEEEEEEEEEEEEEEEE--',
+            success:function(data){ 
+                _this._clear(upload_location);
+                success(data);
+            },
+            error:function(e){
+                _exlog(e); 
+                success({}); 
+            }
+        }); 
+    }
+    _this.del=function(upload_location,success){
+        var url = upload_location.replace(/\/content\/$/,'');
+          $.ajax({
+            url: url +'?access_token='+ token,
+            type:'DELETE',    
+            success:function(data){ 
+                _this._clear(upload_location);
+                success(data);
+            },
+            error:function(e){
+                _exlog(e); 
+                success({}); 
+            }
+         }); 
+    }
+    _this._clear=function(upload_location){ 
+        _exlog("CLEAR!!!"+upload_location);
+        delete localStorage['favorites'];
+        for(var k in localStorage) {
+            if(k.search("https:")<0)continue;
+            delete localStorage[k];
+        }  
+        for(var k in this.load) {
+            if(k.search("https:")<0)continue;
+            delete this.load[k];
+        } 
+        for(var k in this.goto) {
+            if(k.search("https:")<0)continue;
+            delete this.goto[k];
+        } 
+    }
+} 
