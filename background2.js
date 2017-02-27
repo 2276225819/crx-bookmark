21ad928d1fba8b9318ecb6d962517c25ff550d3e
2276225819@qq.com <2276225819@qq.com>
2017-02-27 12:57:39 +0800

    loginAsync

--- background2.js
+++ background2.js
@@ -1,10 +1,11 @@
 var $$ = chrome.rumtime || chrome.extension;
 $$.onMessage.addListener(function(args, sender, sendResponse) {
 	if(!args.shift)return ;
-	console.log(args);
+	console.log(["request:",args]);
 	var id = args.shift();
 	var fn = args.shift();
 	window[fn] && window[fn].apply(null,args).then(function(d){ 
+		console.log(["response:",args]);
 		$$.sendMessage({id:id,data:d}); 
 	}); 
 });
